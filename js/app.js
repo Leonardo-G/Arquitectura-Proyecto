5955c2054aba0e87674a1d9f7b5bb6b5cde22faa
Leonardo Guanuco <guanuco43@gmail.com>
2021-06-09 19:22:35 -0300

    Proyecto-Arquix Guanuco v1.1

--- js/app.js
+++ js/app.js
@@ -1,39 +1,17 @@
-const comentarios = document.querySelectorAll('.comentario__cuadricula');
-const comentarioFotoIzq = document.querySelectorAll('.comentario .r-izq');
-const comentarioFotoDer = document.querySelectorAll('.comentario .r-der');
-// document.addEventListener("DOMContentLoaded", function(){
-//     eventos()
-// })
+window.addEventListener("scroll",function(){
+    let servicios = document.querySelector("#Nosotros");
+    let posicion = servicios.getBoundingClientRect().top;
+    const flecha = document.querySelector(".campoInicio")
+    if(posicion < 0 ){
+        flecha.style.opacity = "1"
+    }else{
+        flecha.style.opacity = "0"
+    }
 
-comentarios[0].addEventListener("mouseover",function(){
-    comentarioFotoIzq[0].style.transform = "translateX(0%)";
-    comentarios[0].onmouseout = function(){
-        comentarioFotoIzq[0].style.transform = "translateX(-100%)";
+    //video
+    const video = document.querySelector("#video");
+    const videoAparacion = video.getBoundingClientRect().top;
+    if(videoAparacion < 1100){
+        video.style.transform = "translate(-50%,-50%)";
     }
 })
-comentarios[2].addEventListener("mouseover",function(){
-    comentarioFotoIzq[1].style.transform = "translateX(0%)";
-    comentarios[2].onmouseout = function(){
-        comentarioFotoIzq[1].style.transform = "translateX(-100%)";
-    }
-})
-comentarios[3].addEventListener("mouseover",function(){
-    comentarioFotoDer[1].style.transform = "translateX(0%)";
-    comentarios[3].onmouseout = function(){
-        comentarioFotoDer[1].style.transform = "translateX(100%)";
-    }
-})
-comentarios[1].addEventListener("mouseover",function(){
-    comentarioFotoDer[0].style.transform = "translateX(0%)";
-    comentarios[1].onmouseout = function(){
-        comentarioFotoDer[0].style.transform = "translateX(100%)";
-    }
-})
-
-
-// comentarios.addEventListener('mouseover', function(){
-//     comentarioFotoIzq.style.transform = "translateX(0%)";
-//     comentario.onmouseout = function(){
-//         comentarioFotoIzq.style.transform = "translateX(-100%)"
-// }
-// })
